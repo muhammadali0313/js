eb2bf5d95020b28d4f23ab7033fafa6dc2799287
Muhammad Ali <153639747+muhammadali0313@users.noreply.github.com>
2024-02-28 02:10:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+alert( 'hello World');
+alert(sum+" Sum of two numbers");
+console.log('hello world')
+var originalNum = 15
+var addnum = 23
+var total = originalNum + addnum
+console.log (originalNum + addnum)
+console.log (total*originalNum)
+console.log (total/addnum*originalNum-total)
+
+// Strings
+alert ("String Starts")
+var name = "Ali"
+var fullname = "Ali abid"
+var age = "17"
+console.log (name)
+console.log (fullname)
+console.log (age)
+
+// Math Expressions Plus Minus
+var one = 10
+var two = 20
+var three = 30
+var four = 40
+var five = 50
+alert (5 + 10 * 23)
+console.log (one + two + three + four + five)
+alert (one * three + four / two)
+
+// Boolean
+
+var t = (true)
+var f = (false)
+alert (t)
+console.log (t)
+console.log (f)
+alert (f)
